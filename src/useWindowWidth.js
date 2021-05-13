c100e9a811cd6138fc1ee5ed6df3882b66fe45ce
Alex <alessandroweb@gmail.com>
2021-05-13 14:33:17 +0800

    added comments and fixed initial state

--- src/useWindowWidth.js
+++ src/useWindowWidth.js
@@ -1,18 +1,25 @@
 import { useState, useEffect } from 'react';
 
 const useWindowWidth = () => {
-  const [WindowWidth, setWindowWidth] = useState(window.innerWidth);
+  // Initialize state with undefined width so server and client renders match
+  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+  const [WindowWidth, setWindowWidth] = useState(undefined);
 
+  // Handler to call on window resize
   const resizeEvent = () => {
     setWindowWidth(window.innerWidth)
   }
 
   useEffect(() => {
+    // Add event listener
     window.addEventListener('resize', resizeEvent);
-    return () => {
-      window.removeEventListener('resize', resizeEvent);
-    };
-  }, [])
+
+    // Call handler right away so state gets updated with initial window size
+    resizeEvent();
+
+    // Remove event listener on cleanup
+    return () => window.removeEventListener('resize', resizeEvent);
+  }, []);  // Empty array ensures that effect is only run on mount
 
   return WindowWidth;
 }
